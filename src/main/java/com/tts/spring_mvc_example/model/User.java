c7ba6b4f9f980c6349dd09b568a07398a4dd7982
cbush2019 <cbush5400@gmail.com>
2020-04-13 22:44:55 -0400

    spring mvc hw

---
+++ src/main/java/com/tts/spring_mvc_example/model/User.java
@@ -0,0 +1,15 @@
+package com.tts.spring_mvc_example.model;
+
+public class User {
+
+	private String userName;
+	
+	public String getUserName() { 
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+ 
+		this.userName = userName; 
+	}
+}
